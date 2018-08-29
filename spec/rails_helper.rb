b22ea4eadad037bf0e788ea4fec41945353414dc
jtrtj <jt.r@live.com>
2018-08-28 21:17:35 -0600

    add require 'vcr' to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,14 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'vcr'
 # Add additional requires below this line. Rails is not loaded until this point!
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data("<GITHUB_TOKEN>") { ENV['jtrtj_test_token'] }
+  config.configure_rspec_metadata!
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
