a25fe14f50e7da3dff7a0d748b875157cbd1e134
jtrtj <jt.r@live.com>
2018-08-29 10:19:59 -0600

    pass fuel station model test

--- spec/models/fuel_station_spec.rb
+++ spec/models/fuel_station_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 describe FuelStation do
-  it 'has attributes' do
-    attributes = JSON.parse('/fixtures/mock_station.json')
-    require 'pry'; binding.pry
+  it 'can create attributes from API json' do
+    mock_station = File.read('fixtures/mock_station.json')
+    attributes = JSON.parse(mock_station, symbolize_names: true)
+    
+    station = FuelStation.new(attributes)
+
+    expect(station.name).to eq(attributes[:station_name])
+    expect(station.address).to eq(attributes[:street_address])
+    expect(station.fuel_types).to eq(attributes[:fuel_type_code])
+    expect(station.distance).to eq(attributes[:distance])
+    expect(station.access_times).to eq(attributes[:access_days_time])
   end
 end
