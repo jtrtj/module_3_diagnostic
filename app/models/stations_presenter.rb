e47fa37111dcacab6649e676f31a03d80b8bbdb7
jtrtj <jt.r@live.com>
2018-08-29 11:01:26 -0600

    refactor stations presenter all tests still green

--- app/models/stations_presenter.rb
+++ app/models/stations_presenter.rb
@@ -10,15 +10,8 @@ class StationsPresenter
   end
 
   private
-  def nrel_conn
-    conn = Faraday.new(:url => "https://developer.nrel.gov/api/alt-fuel-stations/v1/") do |faraday|
-      faraday.adapter  Faraday.default_adapter
-    end
-  end
 
   def nrel_response
     NrelZipSearch.find_stations(@zip_code)
-    # json_response = nrel_conn.get "nearest.json?fuel_type=LPG,ELEC&location=#{@zip_code}&radius=6.0&limit=10&api_key=#{ENV['enrel_api_key']}&format=JSON"
-    # response = JSON.parse(json_response.body, symbolize_names: true)
   end
 end
