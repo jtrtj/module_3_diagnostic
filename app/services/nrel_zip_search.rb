f30d608a9b132ae1564387171e3a09ba40680d15
jtrtj <jt.r@live.com>
2018-08-29 11:06:58 -0600

    further refactor zip search and start nrel api service test

--- app/services/nrel_zip_search.rb
+++ app/services/nrel_zip_search.rb
@@ -1,11 +1,16 @@
 class NrelZipSearch
   def self.find_stations(zip_code)
-    json_response = nrel_conn.get "nearest.json?fuel_type=LPG,ELEC&location=#{zip_code}&radius=6.0&limit=10&api_key=#{ENV['enrel_api_key']}&format=JSON"
-    response = JSON.parse(json_response.body, symbolize_names: true)
-    response[:fuel_stations]
+    response(zip_code)[:fuel_stations]
   end
 
   private
+  def self.response(zip_code)
+    JSON.parse(search_nearest_by_zip(zip_code).body, symbolize_names: true)
+  end
+
+  def self.search_nearest_by_zip(zip_code)
+    nrel_conn.get "nearest.json?fuel_type=LPG,ELEC&location=#{zip_code}&radius=6.0&limit=10&api_key=#{ENV['enrel_api_key']}&format=JSON"
+  end
 
   def self.nrel_conn
     conn = Faraday.new(:url => "https://developer.nrel.gov/api/alt-fuel-stations/v1/") do |faraday|
---
+++ spec/services/nrel_api_service_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe NrelApiServiceSpec do
+  it 'can make a connection to the NREL API' do
+  end
+end
